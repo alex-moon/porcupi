c44d92ba810ebc7e2d48c431b07f8a3ce429b8af
Alex Moon <alex.moon@tangentlabs.co.uk>
2015-05-31 14:08:19 +0100

    Beginnings - the asynchronous thing makes it tricky... perhaps a timeout?

--- src/com/github/alex_moon/porcupi/shell/ShellClient.java
+++ src/com/github/alex_moon/porcupi/shell/ShellClient.java
@@ -1,10 +1,12 @@
 package com.github.alex_moon.porcupi.shell;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+import jline.console.ConsoleReader;
+import jline.console.CursorBuffer;
+import jline.console.completer.StringsCompleter;
 
 import com.github.alex_moon.porcupi.Config;
 import com.github.alex_moon.porcupi.Tellable;
@@ -15,18 +17,20 @@ import com.github.alex_moon.porcupi.transport.Transport;
 public class ShellClient implements Tellable {
     private String inputLine;
     private String context;
-
+    private String prompt;
+    private ConsoleReader console;
+    
     public ShellClient() {
         Socket socket;
         Transport transport;
         try {
             String action, tokens;
             socket = new Socket("127.0.0.1", Config.managerPort);
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             transport = new Transport(socket, this);
             transport.start();
-            // @todo use jline - do this properly
-            while ((inputLine = stdIn.readLine()) != null) {
+
+            setupConsole();
+            while ((inputLine = console.readLine()) != null) {
                 if (!inputLine.trim().equals("")) {
                     if (context != null) {
                         action = context;
@@ -44,40 +48,80 @@ public class ShellClient implements Tellable {
                             message.setContext(context);
                         }
                         transport.tellOut(message);
+                        console.setPrompt("");
                     } catch (MessageException e) {
-                        System.out.println("Bad command: "  + e.getMessage());
+                        debug("Bad command: "  + e.getMessage());
                     }
                 }
             }
             socket.close();
-        } catch (UnknownHostException e1) {
-            System.out.println("Couldn't find 127.0.0.1, viz. you've broken shit you stupid noob");
+        } catch (UnknownHostException e) {
+            debug("Couldn't find 127.0.0.1...");
+            e.printStackTrace();
             System.exit(2);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(2);
         }
     }
+    
+    private void setPrompt(String prompt) {
+        this.prompt = prompt;
+        console.setPrompt(prompt);
+    }
+    
+    private void setupConsole() throws IOException {
+        console = new ConsoleReader();
+        console.setPrompt("porcupi> ");
+        console.addCompleter(new StringsCompleter(Message.actions));
+    }
+    
+    private void setContext(String inputContext) {
+        context = inputContext;
+        if (context != null) {
+            console.setPrompt("porcupi:" + context + "> ");
+        } else {
+            console.setPrompt("porcupi> ");
+        }
+    }
 
     public void tellIn(Message input) {
         if (input.getContext() != null) {
             if (!input.getContext().equals(context)) {
-                System.out.println("now operating in context " + input.getContext());
+                debug("now operating in context " + input.getContext());
             }
         } else {
             if (context != null) {
                 // @todo not really convinced here - why close the context?
-                System.out.println("closing context " + context);
+                debug("closing context " + context);
             }
         }
         // @todo surely we can handle multiple contexts
-        context = input.getContext();
-        System.out.println("received from server: " + input.toString());
+        setContext(input.getContext());
+        debug("received from server: " + input.toString());
+        console.setPrompt(prompt);
     }
 
     public void tellOut(Message output) {
         // if we're at the end of the chain, this method won't have meaning
         // @todo make Teller abstract and implement a stub?
-        System.out.println("HOW DID THIS HAPPEN?: " + output.toString());
+        debug("HOW DID THIS HAPPEN?: " + output.toString());
+    }
+    
+    private void resetConsole() throws IOException {
+        CursorBuffer cursorBuffer = console.getCursorBuffer();
+        console.resetPromptLine(console.getPrompt(), cursorBuffer.buffer.toString(), cursorBuffer.cursor);
+    }
+    
+    private void debug(String messageString) {
+        try {
+            console.println("debug: " + messageString);
+            resetConsole();
+            console.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("CONSOLE IS BROKEN: " + messageString);
+            System.exit(2);
+        }
     }
 }
